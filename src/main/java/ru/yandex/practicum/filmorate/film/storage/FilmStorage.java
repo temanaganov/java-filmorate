fcbb953aacf290b2c90b6615f055e50b2b4a7d88
baser <baserph@yandex.ru>
2022-12-23 13:08:57 +0300

    Заметил ещё лишние пробелы"

--- src/main/java/ru/yandex/practicum/filmorate/film/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/film/storage/FilmStorage.java
@@ -6,22 +6,13 @@ import java.util.List;
 
 public interface FilmStorage {
     List<Film> search(String query, String by);
-
     List<Film> getAll();
-
     List<Film> getAllFilmsByDirectorId(int directorId, String sortBy);
-
     Film getById(int id);
-
     Film create(Film film);
-
     Film update(int id, Film film);
-
     Film delete(int id);
-
     List<Film> getPopularFilms(int count);
-
     void likeFilm(int filmId, int userId);
-
     void deleteLikeFromFilm(int filmId, int userId);
 }
